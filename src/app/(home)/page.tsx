8fb61df9c6779e6b8f8c2b26cc9dfc354e70d90a
Chia1104 <yuyuchia7423@gmail.com>
2025-08-29 21:23:03 +0800

    refactor: refactor routes

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -12,8 +12,8 @@ export default function HomePage() {
       <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
       <p className="text-fd-muted-foreground">
         You can open{" "}
-        <Link href="/docs" className="text-fd-foreground font-semibold underline">
-          /docs
+        <Link href="/docs/about" className="text-fd-foreground font-semibold underline">
+          /about
         </Link>{" "}
         and see the documentation.
       </p>
